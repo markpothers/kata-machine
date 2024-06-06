6e4f3cf0099d6c2531da70b69b28b979e974d197
Mark Pothecary <markpothers@hotmail.com>
2024-06-06 11:57:23 -0500

    maze solved properly

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,82 +1,72 @@
-const isOpen = (
+function isOpen(
+    nextPosition: Point,
     maze: string[],
     wall: string,
-    position: Point,
-    previousPositions: Point[],
-) => {
-    // if is outside of maze
+    path: Point[],
+) {
+    // if outside the maze - false
     if (
-        position.x < 0 ||
-        position.x > maze[0].length ||
-        position.y < 0 ||
-        position.y > maze.length
+        nextPosition.x < 0 ||
+        nextPosition.x >= maze[0].length ||
+        nextPosition.y < 0 ||
+        nextPosition.y >= maze.length
     ) {
         return false;
     }
 
-    // is a wall
-    if (maze[position.y][position.x] === wall) {
+    // if is a wall - false
+    if (maze[nextPosition.y][nextPosition.x] === wall) {
         return false;
     }
 
-    //if you've moved back to the previous position
-    const previousVisit = previousPositions.find(
-        (pos) => pos.x === position.x && pos.y === position.y,
-    );
-    if (previousVisit) {
+    // if is in path - false
+    if (
+        path.some(
+            (step) => step.x === nextPosition.x && step.y === nextPosition.y,
+        )
+    ) {
         return false;
     }
 
     return true;
-};
+}
 
-function recurse(
-    maze: string[],
-    wall: string,
-    end: Point,
-    previousPositions: Point[],
-): Point[] {
-    const currentPosition = previousPositions[previousPositions.length - 1];
-    const choices = [
+function walk(maze: string[], wall: string, end: Point, path: Point[]) {
+    const currentPosition = path[path.length - 1];
+
+    if (currentPosition.x === end.x && currentPosition.y === end.y) {
+        return true;
+    }
+
+    const moveOptions = [
         { x: currentPosition.x, y: currentPosition.y - 1 },
         { x: currentPosition.x + 1, y: currentPosition.y },
         { x: currentPosition.x, y: currentPosition.y + 1 },
         { x: currentPosition.x - 1, y: currentPosition.y },
     ];
 
-    const isEnd = choices.find(
-        (choice) => choice.x === end.x && choice.y === end.y,
-    );
-    if (isEnd) {
-        return [currentPosition, end];
+    for (let i = 0; i < moveOptions.length; i++) {
+        if (isOpen(moveOptions[i], maze, wall, path)) {
+            path.push(moveOptions[i]);
+            const reachedEnd = walk(maze, wall, end, path);
+            if (reachedEnd) {
+                return true;
+            } else {
+                path.pop();
+            }
+        }
     }
 
-for(let i = 0; i<choices.length;i++){
-
-}
-
-    const nextStep = choices.find((choice) =>
-        isOpen(maze, wall, choice, previousPositions),
-    );
-
-    if (nextStep) {
-        return [
-            currentPosition,
-            ...recurse(maze, wall, end, [...previousPositions, nextStep]),
-        ];
-    } else{
-      return [previousPositions[previousPositions.length - 1], ...recurse(maze, wall, end, [...previousPositions.slice(-2)])];
-    }
-    return [];
+    return false;
 }
 
 /**
  * Recursively try directions and push a new point until you find the end.
  * From your current point, calculate the movement options (n, e, s, w).
- * In order, check if each space is open (not a wall, and not seen before). If it is, push and recurse.
+ * In order, check if each space is open (inside the maze, not a wall, and not seen before). If it is, push and recurse.
  * if the new point is the end, return.
- * If no point is available pop the last coordinate of the array and continue the recursion with untried
- * movement options.
+ * Check to see if the recursion returned true. If it did you made it.  return.
+ * If it didn't 'pop' path, allowing previous loops to continue.
  */
 export default function solve(
     maze: string[],
@@ -84,5 +74,8 @@ export default function solve(
     start: Point,
     end: Point,
 ): Point[] {
-    return recurse(maze, wall, end, [start]);
+    const path: Point[] = [start];
+
+    walk(maze, wall, end, path);
+    return path;
 }
